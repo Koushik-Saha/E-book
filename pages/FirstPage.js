c68d8290f364cd332986555265fca55d64c69122
Koushik Saha <koushik.saha666@gmail.com>
2022-02-27 08:07:59 +0600

    add first page\

--- pages/FirstPage.js
+++ pages/FirstPage.js
@@ -1,15 +1,36 @@
 import * as React from 'react';
-import { Button, View, Text, SafeAreaView } from 'react-native';
-import {Box} from "native-base";
+import { NativeBaseProvider, Box, HStack, VStack, Text, Pressable, Image, Center } from 'native-base';
+import {SafeAreaView} from "react-native";
 
 const FirstPage = ({ navigation }) => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <View style={{ flex: 1, padding: 16 }}>
-                <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-                    <Text>Open up App.js to start working on your app!</Text>
+
+            <Center flex={1} px="3">
+                <Box bg="primary.600" py="4" px="3" rounded="md" width={375} maxWidth="100%">
+                    <HStack justifyContent="space-between">
+                        <Box justifyContent="space-between">
+                            <VStack space="2">
+                                <Text fontSize="sm" color="white">
+                                    Today @ 9PM
+                                </Text>
+                                <Text color="white" fontSize="xl">
+                                    Let's talk about avatar!
+                                </Text>
+                            </VStack>
+                            <Pressable rounded="xs" bg="primary.400" alignSelf="flex-start" py="1" px="3">
+                                <Text textTransform="uppercase" fontSize="sm" fontWeight="bold" color="white">
+                                    Remind me
+                                </Text>
+                            </Pressable>
+                        </Box>
+                        <Image source={{
+                            uri: 'https://media.vanityfair.com/photos/5ba12e6d42b9d16f4545aa19/3:2/w_1998,h_1332,c_limit/t-Avatar-The-Last-Airbender-Live-Action.jpg'
+                        }} alt="Aang flying and surrounded by clouds" height="100" rounded="full" width="100" />
+                    </HStack>
                 </Box>
-                <View
+            </Center>
+                {/*<View
                     style={{
                         flex: 1,
                         alignItems: 'center',
@@ -33,8 +54,7 @@ const FirstPage = ({ navigation }) => {
                             title="Go to Third Page"
                         />
                     </Text>
-                </View>
-            </View>
+                </View>*/}
         </SafeAreaView>
     );
 };
